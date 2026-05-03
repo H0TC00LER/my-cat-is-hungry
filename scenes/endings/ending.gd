39154aaa9932b1d0796a452ec02e4fefe73f4113
H0TC00LER <112895111+H0TC00LER@users.noreply.github.com>
2026-05-03 03:20:43 +0300

    progress

--- scenes/endings/ending.gd
+++ scenes/endings/ending.gd
@@ -1,8 +1,16 @@
 extends CanvasLayer
 
+@onready var texture_rect: TextureRect = $MarginContainer/TextureRect
+
 var endings: Array = [
 	{
-		"text": [""],
+		"text": ["Мир снаружи мертв уже много лет.",
+		"Но в конце концов...",
+		"От судьбы не убежать, не правда ли...",
+		"Прости Муся, но я должен идти.",
+		"Мне жаль, что с тобой случилось, но нам обоим будет лучше, если всё это закончится.",
+		"Концовка Ноль: Прощай Муся"
+		],
 		"image": preload("res://assets/ending pictures/0.png")
 	}
 	]
@@ -12,4 +20,6 @@ func _ready() -> void:
 	var ending_number = EventManager.ending
 	var text = endings[ending_number]["text"]
 	var picture = endings[ending_number]["image"]
-	DialogManager.show_dialog(["попа", "и жопа", "офрыаолфаолфраолфра"])
+	DialogManager.show_dialog(text)
+	texture_rect.texture = picture
+	
